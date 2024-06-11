931cf4a2d455d29247ece6a3ede9f680cb5de906
Hien Huynh <hqhienitus@gmail.com>
2024-06-11 13:18:06 +0700

    refactor

--- TestReport.py
+++ TestReport.py
@@ -1,12 +1,40 @@
-import streamlit as st
+@st.cache_data
+def get_UN_data():
+    AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
+    return df.set_index("Region")
 
-dataExploration = st.container()
+try:
+    df = get_UN_data()
+    countries = st.multiselect(
+        "Choose countries", list(df.index), ["China", "United States of America"]
+    )
+    if not countries:
+        st.error("Please select at least one country.")
+    else:
+        data = df.loc[countries]
+        data /= 1000000.0
+        st.write("### Gross Agricultural Production ($B)", data.sort_index())
 
-with dataExploration:
-  st.title('Transport for London')
-  st.subheader('Keeping London moving...')
-  st.header('Dataset: Transport for London')
-  st.markdown('I found this dataset at... https://tfl.gov.uk/info-for/open-data-users/')
-  st.markdown('**Basically, it is a "London AccidentStats" dataset for the year 2019**')
-  st.text('Below is the sample DataFrame')
-  st.write(api_data_modified.head())
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=["index"]).rename(
+            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+        )
+        chart = (
+            alt.Chart(data)
+            .mark_area(opacity=0.3)
+            .encode(
+                x="year:T",
+                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+                color="Region:N",
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except URLError as e:
+    st.error(
+        """
+        **This demo requires internet access.**
+        Connection error: %s
+    """
+        % e.reason
+    )
